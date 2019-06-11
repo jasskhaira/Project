dac6e4f9b51c1ac35b02adf8c96b9705141f110d
jasskhaira <jaspreetsinghkhaira@gmail.com>
2019-06-11 03:50:25 -0400

    --

--- Lpc54114 mcu c code /Test/pwm and usart rtos/source/pwm and usart rtos.c
+++ Lpc54114 mcu c code /Test/pwm and usart rtos/source/pwm and usart rtos.c
@@ -1,37 +1,3 @@
-/*
- * Copyright 2016-2018 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
-/**
- * @file    pwm and usart rtos.c
- * @brief   Application entry point.
- */
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,10 +5,43 @@
 #include "clock_config.h"
 #include "LPC54114_cm4.h"
 #include "fsl_debug_console.h"
+#include "fsl_usart_freertos.h"
+#include "fsl_usart.h"
 /* TODO: insert other include files here. */
 
 /* TODO: insert other definitions and declarations here. */
+#define DEMO_USART USART0
+#define DEMO_USART_IRQHandler FLEXCOMM0_IRQHandler
+#define DEMO_USART_IRQn FLEXCOMM0_IRQn
+/* Task priorities. */
+#define uart_task_PRIORITY (configMAX_PRIORITIES - 1)
+#define USART_NVIC_PRIO 5
 
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+static void uart_task(void *pvParameters);
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+const char *to_send = "FreeRTOS USART driver example!\r\n";
+const char *send_buffer_overrun = "\r\nRing buffer overrun!\r\n";
+uint8_t background_buffer[32];
+uint8_t recv_buffer[4];
+
+usart_rtos_handle_t handle;
+struct _usart_handle t_handle;
+
+struct rtos_usart_config usart_config = {
+		.base = DEMO_USART,
+		.srcclk = BOARD_DEBUG_UART_CLK_FREQ,
+		.baudrate = 115200,
+    .parity = kUSART_ParityDisabled,
+    .stopbits = kUSART_OneStopBit,
+    .buffer = background_buffer,
+    .buffer_size = sizeof(background_buffer),
+};
 /*
  * @brief   Application entry point.
  */
@@ -55,13 +54,28 @@ int main(void) {
   	/* Init FSL debug console. */
     BOARD_InitDebugConsole();
 
-    PRINTF("Hello World\n");
+    USART_RTOS_Init(&handle, &t_handle, &usart_config);
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-    }
-    return 0 ;
+    if (xTaskCreate(uart_task, "Uart_task", configMINIMAL_STACK_SIZE + 10, NULL, uart_task_PRIORITY, NULL) != pdPASS)
+      {
+          PRINTF("Task creation failed!.\r\n");
+          while (1)
+              ;
+      }
+      vTaskStartScheduler();
+      for (;;)
+          ;
+  }
+
+
+static void uart_task(void *pvParameters)
+{
+
+	 if (0 > USART_RTOS_Send(&handle, (uint8_t *)to_send, strlen(to_send)))
+	    {
+	        vTaskSuspend(NULL);
+	    }
+	 vTaskSuspend(NULL);
+
 }
+
